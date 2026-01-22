817d43f65d619e81ea01285f63ca6f7ff92251be
Pagsshines33 <151986176+Pagsshines33@users.noreply.github.com>
2026-01-22 13:42:54 +0530

    Update Program10.swift

--- Programs_On_Strings/Program10.swift
+++ Programs_On_Strings/Program10.swift
@@ -1,3 +1,5 @@
+//Given a string of words separated by a " ". Write code that prints out the length of the last word.
+
 let text = "Swift is very powerful"
 let words = text.split(separator: " ")
 

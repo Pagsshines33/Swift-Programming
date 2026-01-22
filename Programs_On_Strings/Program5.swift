97cd3017e03feeba5638f1ba3dc3b6fb9fbf91e6
Pagsshines33 <151986176+Pagsshines33@users.noreply.github.com>
2026-01-22 13:40:32 +0530

    Update Program5.swift

--- Programs_On_Strings/Program5.swift
+++ Programs_On_Strings/Program5.swift
@@ -1,3 +1,5 @@
+//You are given a string stored in variable aString. Create a new string called reverse that contains the original string in reverse order. Print the reversed string. You cannot used built-in reverse.
+
 let aString = "Swift"
 var reverse = ""
 

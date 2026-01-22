3b91a9d24d3af5711d66c0b34e92f9eacfcf5921
Pagsshines33 <151986176+Pagsshines33@users.noreply.github.com>
2026-01-22 13:41:33 +0530

    Update Program7.swift

--- Programs_On_Strings/Program7.swift
+++ Programs_On_Strings/Program7.swift
@@ -1,3 +1,5 @@
+//You are given a string stored in variable problem. Write code so that you print each word of the string on a new line.
+
 let problem = "Swift is easy and powerful"
 let words = problem.split(separator: " ")
 

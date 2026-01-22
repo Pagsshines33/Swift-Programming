c8769057862fc82ddcb101bfab0a512b320424f3
Pagsshines33 <151986176+Pagsshines33@users.noreply.github.com>
2026-01-22 13:42:06 +0530

    Update Program8.swift

--- Programs_On_Strings/Program8.swift
+++ Programs_On_Strings/Program8.swift
@@ -1,3 +1,5 @@
+//You are given a string stored in variable problem. Write code that prints the longest word in the string.
+
 let problem = "Swift programming language is powerful"
 let words = problem.split(separator: " ")
 
